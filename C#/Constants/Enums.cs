4c70ed08ba155faa9211a2ede2614056f9ac686c
TABLE1 <TABLE1@univ-tln.fr>
2021-04-28 13:22:53 +0200

    Invalid dictionary in Constants 2

--- C#/Constants/Enums.cs
+++ C#/Constants/Enums.cs
@@ -6,16 +6,24 @@ using System.Threading.Tasks;
 
 namespace Constants
 {
-    public enum Functions : ushort
+    public class Enums
     {
-        LED_PROTOCOL = 0x0020,
-        TELEMETER_PROTOCOL = 0x0030,
-        MOTOR_PORTOCOL = 0x0040,
-        STATE_PROTOCOL = 0x0050,
-        SET_ROBOT_STATE = 0x0051,
-        SET_ROBOT_MANUAL_CONTROL = 0x0052,
-        POSITION_DATA = 0x0061,
-        MESSAGE_PROTOCOL = 0x0080,
+        public Enums()
+        {
+
+        }
+
+        public enum Functions : ushort
+        {
+            LED_PROTOCOL = 0x0020,
+            TELEMETER_PROTOCOL = 0x0030,
+            MOTOR_PORTOCOL = 0x0040,
+            STATE_PROTOCOL = 0x0050,
+            SET_ROBOT_STATE = 0x0051,
+            SET_ROBOT_MANUAL_CONTROL = 0x0052,
+            POSITION_DATA = 0x0061,
+            MESSAGE_PROTOCOL = 0x0080,
+        }
     }
 
 }
