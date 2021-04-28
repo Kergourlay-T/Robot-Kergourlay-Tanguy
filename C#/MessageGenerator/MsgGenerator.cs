2cd67f73ebf8f7340d04fcef154455dd8cbf10b2
TABLE1 <TABLE1@univ-tln.fr>
2021-04-28 11:26:07 +0200

    created MessageGenerator Project

---
+++ C#/MessageGenerator/MsgGenerator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageGenerator
+{
+    public class MsgGenerator
+    {
+        public MsgGenerator()
+        {
+            OnMessageGeneratorCreated();
+        }
+        public event EventHandler<EventArgs> OnMessageGeneratorCreatedEvent;
+        public virtual void OnMessageGeneratorCreated()
+        {
+            OnMessageGeneratorCreatedEvent?.Invoke(this, new EventArgs());
+        }
+
+
+        public void GenerateMessageLEDSetStateConsigneToRobot(ushort numberLED, bool LEDState)
+        {
+            byte[] payload = new byte[] { (byte)numberLED, (byte)(LEDState ? 0x00 : 0x01) };
+            Serial.MsgEncoder.UartEcodeAndSendMessage((ushort)Protocol.Functions.LED_PROTOCOL, payload);
+        }
+
+        public void GenerateMessageMotorSetSpeedToRobot(byte leftMotorSpeed, byte rigthMotorSpeed)
+        {
+            byte[] payload = new byte[2];
+            if (100 >= leftMotorSpeed && leftMotorSpeed <= -100)
+            {
+                payload[0] = (byte)leftMotorSpeed;
+            }
+            else
+            {
+                payload[0] = (byte)((leftMotorSpeed > 0) ? 100 : -100);
+            }
+            if (100 >= rigthMotorSpeed && rigthMotorSpeed <= -100)
+            {
+                payload[1] = (byte)rigthMotorSpeed;
+            }
+            else
+            {
+                payload[1] = (byte)((rigthMotorSpeed > 0) ? 100 : -100);
+            }
+            Serial.MsgEncoder.UartEcodeAndSendMessage((ushort)Protocol.Functions.MOTOR_PORTOCOL, payload);
+        }
+
+        public void GenerateMessageSetStateRobot(byte StateRobot)
+        {
+            byte[] payload = new byte[] { (byte)StateRobot };
+            Serial.MsgEncoder.UartEcodeAndSendMessage((ushort)Protocol.Functions.SET_ROBOT_STATE, payload);
+        }
+
+    }//End MsgGenerator
+}//End Message Generator
---
+++ C#/MessageGenerator/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Les informations générales relatives à un assembly dépendent de
+// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+// associées à un assembly.
+[assembly: AssemblyTitle("MessageGenerator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MessageGenerator")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly
+// aux composants COM. Si vous devez accéder à un type dans cet assembly à partir de
+// COM, affectez la valeur true à l'attribut ComVisible sur ce type.
+[assembly: ComVisible(false)]
+
+// Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+[assembly: Guid("7d0cde19-54c1-45e2-82c4-006f718415ad")]
+
+// Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+//
+//      Version principale
+//      Version secondaire
+//      Numéro de build
+//      Révision
+//
+// Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut
+// en utilisant '*', comme indiqué ci-dessous :
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
