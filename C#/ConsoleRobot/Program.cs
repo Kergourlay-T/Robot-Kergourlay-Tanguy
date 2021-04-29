c84232c5d9cc85ec0d8de4ab11545318782ea64c
TABLE1 <TABLE1@univ-tln.fr>
2021-04-29 18:30:56 +0200

    Program in process

--- C#/ConsoleRobot/Program.cs
+++ C#/ConsoleRobot/Program.cs
@@ -4,10 +4,104 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using MessageDecoder;
+using MessageEncoder;
+using MessageGenerator;
+using MessageProcessor;
+using Serial;
+
 namespace ConsoleRobot
 {
     public class Program
     {
+        static Serial serial;
 
-    }
-}
+        static void Main()
+        {
+            ConsoleFormat.ConsoleInformationFormat(Constants.ConsoleTitleFormatConst.MAIN, "Begin Booting Sequence", true);
+
+            serial = new Serial();
+            MessageDecoder.MsgDecoder.OnMessageDecoderCreatedEvent += ConsoleFormat.PrintMessageDecoderCreated;
+            OnMessageEncoderCreatedEvent += ConsoleFormat.PrintMessageEncoderCreated;
+
+
+            #region Event
+            #region Communication 
+            #region Serial
+            //OnSerialConnectedEvent;
+            //OnAutoConnectionLaunchedEvent;
+            //OnNewSerialAttemptEvent;
+            //OnSerialAvailableListEvent;
+            //OnWrongCOMAvailableEvent;
+            //OnCorrectCOMAvailableEvent;
+            //OnSerialAvailableEvent;
+            //OnNoConnectionAvailablEvent;
+            //OnErrorWhileWhileAttemptingCOMEvent;
+            PrintMessageDecoderCreated
+            
+          PrintMessageEncoderCreated
+            
+          PrintMessageProcessorCreated
+           
+            PrintMessageGeneratorCreated
+
+            #endregion
+
+            #region Hex Viewer
+            OnMessageDecoderCreatedEvent;
+            OnSOFByteReceivedEvent;
+            OnFunctionMSBByteReceivedEvent;
+            OnFunctionLSBByteReceivedEvent;
+            OnPayloadLenghtMSBByteReceivedEvent;
+            OnPayloadLenghtLSBByteReceivedEvent;
+            OnPayloadByteReceivedEvent;
+            OnPayloadReceivedEvent;
+            OnChecksumByteReceivedEvent;
+            OnCorrectChecksumReceivedEvent;
+            OnWrongChecksumReceivedEvent;
+            #endregion
+
+            #region Hex Viewer Error
+            OnUnknowByteReceivedEvent;
+            OnUnknowFunctionEvent;
+            OnOverLenghtMessageEvent;
+            OnWrongLenghtEvent;
+            #endregion
+
+            #region Hex Sender
+            OnMessageEncoderCreatedEvent;
+            OnSendMessageEvent;
+            OnSetLEDStateEvent;
+            OnMotorSetSpeedEvent;
+            OnSetStateEvent;
+            #endregion
+
+            #region Hex Sender Error 
+            OnSerialDeconnectedEvent;
+            OnWrongPayloadSendEvent;
+            OnUnknowFunctionSentEvent;
+            #endregion
+
+
+
+
+            #region Processor
+            OnMessageProcessorCreatedEvent;
+            OnIRMessageReceivedEvent;
+            OnMotorMessageReceivedEvent;
+            OnStateMessageReceivedEvent;
+            OnPositionMessageReceivedEvent;
+            OnTextMessageReceivedEvent;
+            OnUnknowFunctionReceivedEvent;
+            #endregion
+
+            #endregion //End region Communication
+            #endregion //End region Event
+
+
+        }
+
+
+    }//End Program
+}//End ConsolerOBOT
+    
--- C#/Constants/ConsoleTitleFormatConst.cs
+++ C#/Constants/ConsoleTitleFormatConst.cs
@@ -8,16 +8,17 @@ namespace Constants
 {
     public static class ConsoleTitleFormatConst
     {
-            public const string DECODER = "DECODER";
-            public const string ENCODER = "ENCODER";
-            public const string GENERATOR = "GENERATOR";
-            public const string PROCESSOR = "PROCESSOR";
-            public const string SERIAL = "SERIAL";
-            public const string IR = "IR";
-            public const string MOTOR = "MOTOR";
-            public const string STATE_ROBOT = "STATE_ROBOT";
-            public const string TEXT = "TEXT";
-            public const string LED = "LED";
-            public const string POSITIONDATA = "POSITION DATA";
+        public const string DECODER = "DECODER";
+        public const string ENCODER = "ENCODER";
+        public const string GENERATOR = "GENERATOR";
+        public const string PROCESSOR = "PROCESSOR";
+        public const string SERIAL = "SERIAL";
+        public const string IR = "IR";
+        public const string MOTOR = "MOTOR";
+        public const string STATE_ROBOT = "STATE_ROBOT";
+        public const string TEXT = "TEXT";
+        public const string LED = "LED";
+        public const string POSITIONDATA = "POSITION DATA";
+        public const string MAIN = "MAIN";        
     }
 }
